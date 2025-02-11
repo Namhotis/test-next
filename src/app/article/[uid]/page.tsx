718504ec1bc209153d2600d04537bdbb43428a18
Mathis-Dyk_wmg <mathis.dyk@warnermusic.com>
2025-02-11 23:09:33 +0100

    remove unused dependency

--- src/app/article/[uid]/page.tsx
+++ src/app/article/[uid]/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
-import { asText, filter } from "@prismicio/client";
+import { filter } from "@prismicio/client";
 import { SliceZone } from "@prismicio/react";
 
 import { createClient } from "@/prismicio";
